d80e97e8524b8d405fa2f07ccaea1bbfb80141ff
julian7454 <julian7454@gmail.com>
2025-06-25 22:56:23 +0800

    chore: Update code style and dependencies

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,9 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-import pluginVitest from '@vitest/eslint-plugin'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import { globalIgnores } from 'eslint/config';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
+import pluginVitest from '@vitest/eslint-plugin';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+import prettier from 'eslint-plugin-prettier-vue';
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -19,10 +20,31 @@ export default defineConfigWithVueTs(
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+    files: ['**/*.vue'],
+  },
+  {
+    plugins: {
+      'prettier-vue': prettier,
+    },
+    rules: {
+      'prettier-vue/prettier': [
+        'error',
+        {
+          semi: true,
+        },
+      ],
+      'vue/valid-v-slot': ['error', { allowModifiers: true }],
+    },
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
   skipFormatting,
-)
+);
