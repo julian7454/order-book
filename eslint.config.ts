1aca34f1ddff6e0da2fd7f0c81ac1339bcb70344
julian7454 <julian7454@gmail.com>
2025-06-27 21:46:00 +0800

    style: Re-indent

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,40 +11,40 @@ import prettier from 'eslint-plugin-prettier-vue';
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
-
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
-
-  {
-    ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
-  },
-  {
-    rules: {
-      'vue/multi-word-component-names': 'off',
+    {
+        name: 'app/files-to-lint',
+        files: ['**/*.{ts,mts,tsx,vue}'],
     },
-    files: ['**/*.vue'],
-  },
-  {
-    plugins: {
-      'prettier-vue': prettier,
+
+    globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+    pluginVue.configs['flat/essential'],
+    vueTsConfigs.recommended,
+
+    {
+        ...pluginVitest.configs.recommended,
+        files: ['src/**/__tests__/*'],
     },
-    rules: {
-      'prettier-vue/prettier': [
-        'error',
-        {
-          semi: true,
+    {
+        rules: {
+            'vue/multi-word-component-names': 'off',
         },
-      ],
-      'vue/valid-v-slot': ['error', { allowModifiers: true }],
+        files: ['**/*.vue'],
     },
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
-  skipFormatting,
+    {
+        plugins: {
+            'prettier-vue': prettier,
+        },
+        rules: {
+            'prettier-vue/prettier': [
+                'error',
+                {
+                    semi: true,
+                },
+            ],
+            'vue/valid-v-slot': ['error', { allowModifiers: true }],
+        },
+        files: ['**/*.{ts,mts,tsx,vue}'],
+    },
+    skipFormatting,
 );
